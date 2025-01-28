4441775b15f6543f8a50ef89f5d4e623f30b1c7c
Itzel Villasenor <itzelvillg@gmail.com>
2025-01-28 07:34:33 -0600

    Check JWT UI and Backend

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,16 +2,31 @@ import { Navigate, Route, Routes } from "react-router"
 import { LoginPage } from "../auth/pages/LoginPage"
 import { CalendarPage } from "../calendar/pages/CalendarPage"
 import { getEnvVariables } from "../helpers/getEnvVariables"
+import { useAuthStore } from "../hooks/useAuthStore"
+import { useEffect } from "react"
 
 
 export const AppRouter = () => {
 
-const authStatus = 'not-authenticated'
-console.log(getEnvVariables())
+
+
+  const {status,checkAuthToken} = useAuthStore();
+
+  useEffect(() => {
+    
+   checkAuthToken();
+  }, []);
+
+
+  if(status === 'checking'){
+    return(
+      <h3>Loading...</h3>
+    )
+  }
   return (
     <Routes>
       {
-      (authStatus === 'not-authenticated')
+      (status === 'not-authenticated')
         ?<Route path="/auth/*" element={<LoginPage/>}/>
         :<Route path="/*" element={<CalendarPage/>}/>
       }
