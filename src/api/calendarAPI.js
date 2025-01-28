423cd7cb57ca348391c5ed18b5c7bf1be7d513c4
Itzel Villasenor <itzelvillg@gmail.com>
2025-01-28 07:22:14 -0600

    Token on headers

--- src/api/calendarAPI.js
+++ src/api/calendarAPI.js
@@ -7,3 +7,15 @@ const {VITE_API_URL} = getEnvVariables();
 export const calendarApi = axios.create({
   baseURL: VITE_API_URL
 });
+
+
+calendarApi.interceptors.request.use(config =>{
+  config.headers = {
+    ...config.headers,
+    'x-tonken' : localStorage.getItem('token')
+  }
+  
+  
+  return config;
+})
+
